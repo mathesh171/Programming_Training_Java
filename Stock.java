5f02c139b0e107ed794e0fe0d710eeee89b08286
MATHESH M <156519287+Alphamale916@users.noreply.github.com>
2024-08-28 10:27:21 +0530

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -49,7 +49,7 @@ public class Stock
 	    }
 	    int k=x.nextInt();
 	    for(int i=0;i<n;i++){
-	        for(int j=i+1;j<n;j++){
+	        for(int j=i+1;j<k;j++){
 	            if(arr[j]<arr[i]){
 	                arr[i]=arr[i]+arr[j];
 	                arr[j]=arr[i]-arr[j];
